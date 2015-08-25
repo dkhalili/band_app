0be397ca9f2f66921c810524e08c49ce8319c352
Xi Zhang <xizhangmit@gmail.com>
2015-08-25 19:42:38 -0400

    date conflict check

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,10 +16,14 @@ class EventsController < ApplicationController
 	end
 
 	def create		
-
-		@event = Event.create(event_params)
-		redirect_to venues_path
-
+		@eventFind = Event.where(:date => params[:event][:date])
+		# binding.pry
+		if @eventFind.length == 0
+			@event = Event.create(event_params)
+			redirect_to venues_path
+		else
+			redirect_to new_venue_event_path
+		end
 	end
 
   	def show
