7ba022809b91f05c95fb8351e24286a37e73bc81
Xi Zhang <xizhangmit@gmail.com>
2015-08-25 16:22:34 -0400

    add venue

---
+++ app/controllers/venues_controller.rb
@@ -0,0 +1,35 @@
+class VenuesController < ApplicationController
+
+	def index
+		@venues = Venue.all
+
+	end
+
+	def show
+		@venue = Venue.find(params[:id])
+	end
+
+	def new
+		@venue = Venue.new
+	end	
+
+	def create
+		# binding.pry
+		@venue = Venue.create(venue_params)
+		redirect_to venues_path
+	end		
+
+	def edit
+	end	
+
+	def update
+	end		
+
+	  private
+	  def venue_params
+	    params
+	      .require(:venue)
+	      .permit(:name, :city, :state, :family_friendly)
+	  end	
+
+end
